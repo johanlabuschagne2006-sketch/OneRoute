ffe9e1d867df5413aa1e7ff7d1471c6808c5decd
johanlabuschagne2006-sketch <johanlabuschagne2006@gmail.com>
2025-08-14 14:21:22 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
-module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer
+  ]
 };
-
-
 
 
 
