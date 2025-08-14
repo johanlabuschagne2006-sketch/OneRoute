fd446675d8263fe3bd9a750a608e78008f68bb41
johanlabuschagne2006-sketch <johanlabuschagne2006@gmail.com>
2025-08-14 14:42:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.', // Root of the project
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'client/src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
-});
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss()
+      ]
+    }
+  }
+})
