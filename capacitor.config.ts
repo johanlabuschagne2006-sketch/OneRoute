6e585c07134a0724dc19a86d9b8f88c1ca0f6e8e
johanlabuschagne2006-sketch <johanlabuschagne2006@gmail.com>
2025-08-14 13:16:39 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.oneroute.app',
   appName: 'One Route',
-  webDir: 'dist/public',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
     // Removed "url" so it uses bundled build instead of external website
